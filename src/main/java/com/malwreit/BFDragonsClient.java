22929b1ccf8db88de5a6ac3f358de805f087a2bf
malwreit <malwreit@gmail.com>
2025-05-29 17:28:19 +0200

    x

---
+++ src/main/java/com/malwreit/BFDragonsClient.java
@@ -0,0 +1,11 @@
+package com.malwreit;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BFDragonsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
