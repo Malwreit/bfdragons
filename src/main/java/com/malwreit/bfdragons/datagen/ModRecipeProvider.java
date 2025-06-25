ffb334ed24c153f705effb75d3ca846276fa227c
malwreit <malwreit@gmail.com>
2025-06-25 08:53:16 +0200

    e25 - Crafting recipe added

--- src/main/java/com/malwreit/bfdragons/datagen/ModRecipeProvider.java
+++ src/main/java/com/malwreit/bfdragons/datagen/ModRecipeProvider.java
@@ -8,6 +8,7 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.Identifier;
@@ -35,6 +36,15 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .input(ModBlocks.STEEL_BLOCK)
                 .criterion(hasItem(ModBlocks.STEEL_BLOCK), conditionsFromItem(ModBlocks.STEEL_BLOCK))
                 .offerTo(recipeExporter, Identifier.of(BFDragons.MOD_ID, "steel_block_from_steel_shapeless"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TRANSPORTATION, ModItems.STEEL_HORSE_ARMOR)
+                .pattern("  R")
+                .pattern("RFR")
+                .pattern("R R")
+                .input('R',ModItems.STEEL)
+                .input('F', Items.IRON_HORSE_ARMOR)
+                .criterion(hasItem(ModItems.STEEL), conditionsFromItem(ModItems.STEEL))
+                .offerTo(recipeExporter, Identifier.of(BFDragons.MOD_ID, "steel_horse_armor_from_steel"));
 
         offerSmithingTrimRecipe(recipeExporter, ModItems.IRON_HILLS_SMITHING_TEMPLATE, Identifier.of(BFDragons.MOD_ID, "iron_hills"));
 
